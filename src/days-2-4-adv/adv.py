f0726658ded7b250a843f08f38d6dcacd8a1da5c
BrianARuff <thelonewolf198927@gmail.com>
2018-11-08 15:09:13 -0500

    fixed loop

--- src/days-2-4-adv/adv.py
+++ src/days-2-4-adv/adv.py
@@ -5,24 +5,23 @@ import sys
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+    'outside': Room("Outside Cave Entrance",
+                    "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+    'foyer': Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+    'narrow': Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
-
 
 # Link rooms together -- I did not use these...
 
@@ -48,6 +47,7 @@ p_name = input("Player Name: ")
 
 p1 = Player(p_name)
 
+
 # Write a loop that:
 #
 # * Prints the current room name DONE
@@ -56,35 +56,31 @@ p1 = Player(p_name)
 
 
 def set_player_direction(player):
-
     print("___PLAYER_ROOM___: " + player.room_name)
     print("___PLAYER_ROOM DESCRIPTION___: " + player.room_description)
 
-    setting_direction = [True]
+# Get input from user to set as direction to be returned.
 
-    while setting_direction:
+    direction = input("Enter a direction [n, s, e, w] or q to quit.").strip().lower()
 
-        # Get input from user to set as direction to be returned.
-        direction = input("Enter a direction [n, s, e, w] or q to quit.").lower()
+    if direction == "n":
+        return direction
 
-        if direction == "n":
-            return direction
+    elif direction == "s":
+        return direction
 
-        elif direction == "s":
-            return direction
+    elif direction == "e":
+        return direction
 
-        elif direction == "e":
-            return direction
+    elif direction == "w":
+        return direction
 
-        elif direction == "w":
-            return direction
+    elif direction == "q":
+        print("Thanks for playing. I hope you had fun!")
+        sys.exit()
 
-        elif direction == "q":
-            print("Thanks for playing. I hope you had fun!")
-            sys.exit()
-        else:
-            print("Invalid choice. Please choose either [n,s,e,w]")
-
+    else:
+        print("Invalid choice. Please choose either [n,s,e,w]")
 
 
 # If the user enters a cardinal direction, attempt to move to the room there. DONE
@@ -94,16 +90,14 @@ def set_player_direction(player):
 
 def play_game():
 
-    playing_game = [True]
-
-    while playing_game:
+    while True:
 
         # Get Direction from set_player_direction function
         direction = set_player_direction(p1)
 
         if direction == "q":
             print("Thanks for playing. I hope you had fun!")
-            sys.exit()
+            break
 
         # Handle Outside to Foyer
         if p1.room_name == 'Outside Cave Entrance' and direction == 'n':
